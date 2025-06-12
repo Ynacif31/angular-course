bbe6348d2238252353593edb8c8297c4693e6030
Ygor Nacif <ygor.nacif00@gmail.com>
2025-06-12 11:27:11 -0300

    Angular material add

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,12 +1,22 @@
 import { Component } from "@angular/core";
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @Component({
     selector: 'app-post-create',
     templateUrl: './post-create.component.html',
     styleUrls: ['./post-create.component.css'],
     standalone: true,
-    imports: [FormsModule]
+    imports: [
+        FormsModule,
+        BrowserAnimationsModule,
+        MatInputModule,
+        MatCardModule,
+        MatButtonModule
+    ]
 })
 export class PostCreateComponent {
 
