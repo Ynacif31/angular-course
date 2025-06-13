f67d1ed6e4b305300551095a56295cb4f2052037
Ygor Nacif <ygor.nacif00@gmail.com>
2025-06-13 10:23:17 -0300

    Diving into Structural Directives

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-post-list',
@@ -7,13 +8,15 @@ import { MatExpansionModule } from '@angular/material/expansion';
     styleUrls: ['./post-list.component.css'],
     standalone: true,
     imports: [
-        MatExpansionModule
+        MatExpansionModule,
+        CommonModule
     ]
 })
 export class PostListComponent {
-    posts = [
-        { title: 'First Post', content: 'This is the first post' },
-        { title: 'Second Post', content: 'This is the second post' },
-        { title: 'Third Post', content: 'This is the third post' }
-    ];
+   //posts = [
+        //{ title: 'First Post', content: 'This is the first post' },
+        //{ title: 'Second Post', content: 'This is the second post' },
+        //{ title: 'Third Post', content: 'This is the third post' }
+    //];
+    posts = [];
 }
