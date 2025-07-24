e97593da028caee6768da4bcbafdde653874ca67
Ynacif31 <ygor.nacif00@gmail.com>
2025-07-24 10:13:03 -0300

    Transforming response data

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Post } from "./post.model";
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -18,8 +19,19 @@ export class PostsService {
 
     getPosts() {
        this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-       .subscribe((postData) => {
-        this.posts = postData.posts;
+       .pipe(map(
+        (postData) => {
+            return postData.posts.map(post => {
+                return {
+                    title: post.title,
+                    content: post.content,
+                    id: post.id
+                };
+            });
+        }
+       ))
+       .subscribe((transformedPosts) => {
+        this.posts = transformedPosts;
         this.postsUpdated.next([...this.posts]);
        });
     }
