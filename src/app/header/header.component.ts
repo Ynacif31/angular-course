99de1b6561c0f8ad645b98f1567d529af33fdc3f
Ynacif31 <ygor.nacif00@gmail.com>
2025-08-04 21:14:41 -0300

    feat: Styling Links

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css'],
     standalone: true,
     imports: [
         MatToolbarModule,
-        RouterModule
+        RouterModule,
+        MatButtonModule
     ]
 })
 export class HeaderComponent {}
